1a266ed41fd0480d11226fbe5e2ad1a40988b85d
allexis096 <allexissoares@gmail.com>
2020-11-23 22:32:49 -0300

    :art: chore: Login page stylized

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { FormEvent, useCallback, useState } from 'react';
+import { FaGithub, FaArrowRight } from 'react-icons/fa';
 
-import { Container } from './styles';
+import { Container, Form } from './styles';
 
 const Login: React.FC = () => {
+  const [user, setUser] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = useCallback(
+    async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      if (!user) {
+        setError('Campo obrigatório');
+      }
+    },
+    [user],
+  );
+
   return (
     <Container>
-      <h1>Login</h1>
+      <FaGithub size={80} color="#FFCE00" />
+
+      <Form Error={!!error} onSubmit={handleSubmit}>
+        <input
+          placeholder="Usuário"
+          value={user}
+          onChange={e => setUser(e.target.value)}
+        />
+        {error && <span>{error}</span>}
+        <button type="submit">
+          ENTRAR
+          <FaArrowRight />
+        </button>
+      </Form>
     </Container>
   );
 };
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,3 +1,64 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+interface FormProps {
+  Error: boolean;
+}
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Form = styled.form<FormProps>`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  input,
+  button {
+    width: 339px;
+    height: 56px;
+    border-radius: 8px;
+    border: none;
+  }
+
+  input {
+    margin: 30px 0 20px;
+    padding-left: 8px;
+  }
+
+  ${props =>
+    props.Error &&
+    css`
+      color: #eb2d2d;
+    `}
+
+  > span {
+    position: absolute;
+    display: block;
+    top: 46px;
+    right: 8px;
+  }
+
+  button {
+    background-color: #ffce00;
+    font-weight: bold;
+    font-size: 20px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      margin-left: 8px;
+    }
+
+    transition: opacity 0.2s;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
