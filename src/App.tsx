acd381b3a98f22f351ef2d8da1bb01e6d7c38e29
allexis096 <allexissoares@gmail.com>
2020-11-24 22:56:11 -0300

    :zap: perf: improve hook useUsers Provider on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { UsersProvider } from './hooks/users';
 
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes />
+    <>
+      <BrowserRouter>
+        <UsersProvider>
+          <Routes />
+        </UsersProvider>
+      </BrowserRouter>
+
       <GlobalStyles />
-    </BrowserRouter>
+    </>
   );
 };
 
