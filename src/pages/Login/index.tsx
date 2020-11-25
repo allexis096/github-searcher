d07ae56b484b6e0fa63b8ebcb65c2e25a801013c
allexis096 <allexissoares@gmail.com>
2020-11-24 23:01:39 -0300

    :rocket: chore: Login page done

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,21 +1,44 @@
-import React, { FormEvent, useCallback, useState } from 'react';
+import React, { FormEvent, useCallback, useEffect, useState } from 'react';
 import { FaGithub, FaArrowRight } from 'react-icons/fa';
+import { useHistory } from 'react-router-dom';
+import { useUsers } from '../../hooks/users';
+import api from '../../services/api';
 
 import { Container, Form } from './styles';
 
 const Login: React.FC = () => {
-  const [user, setUser] = useState('');
+  const { setUser } = useUsers();
+  const history = useHistory();
+  const [gitUser, setGitUser] = useState('');
   const [error, setError] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('GithubUser')) {
+      history.push('/dashboard');
+    }
+  }, [history]);
 
   const handleSubmit = useCallback(
     async (event: FormEvent<HTMLFormElement>) => {
       event.preventDefault();
 
-      if (!user) {
+      if (!gitUser) {
         setError('Campo obrigatório');
+        return;
+      }
+
+      const response = await api.get(`users/${gitUser}`);
+      try {
+        setUser(response.data);
+
+        localStorage.setItem('GithubUser', JSON.stringify(response.data));
+
+        history.push('/dashboard');
+      } catch (err) {
+        setError('Erro ao buscar usuário');
       }
     },
-    [user],
+    [gitUser, setUser, history],
   );
 
   return (
@@ -25,8 +48,8 @@ const Login: React.FC = () => {
       <Form Error={!!error} onSubmit={handleSubmit}>
         <input
           placeholder="Usuário"
-          value={user}
-          onChange={e => setUser(e.target.value)}
+          value={gitUser}
+          onChange={e => setGitUser(e.target.value)}
         />
         {error && <span>{error}</span>}
         <button type="submit">
