62837c88573caaae4e055d55717434651389d3b7
allexis096 <allexissoares@gmail.com>
2020-11-25 19:25:09 -0300

    :zap: chore: using api and function handleClick

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import { Link, useHistory } from 'react-router-dom';
+import { useUsers } from '../../hooks/users';
+import api from '../../services/api';
 
 import { Container, Header, CardFollowing } from './styles';
 
+interface FollowingProps {
+  id: number;
+  login: string;
+  avatar_url: string;
+}
+
 const Following: React.FC = () => {
+  const { user, setNewUser } = useUsers();
+  const [following, setFollowing] = useState<FollowingProps[]>([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    api
+      .get(`/users/${user.login}/following`)
+      .then(response => setFollowing(response.data));
+  }, [history, user.login]);
+
+  const handleClick = useCallback(
+    (githubUser: string) => {
+      setNewUser(githubUser);
+    },
+    [setNewUser],
+  );
+
   return (
     <Container>
       <Header>
-        <FaArrowLeft size={20} />
-        <span>10 seguindo</span>
+        <Link to="/dashboard">
+          <FaArrowLeft size={20} />
+        </Link>
+        <span>{user.following} seguindo</span>
       </Header>
-      <CardFollowing>
-        <main>
-          <div />
-          <img src="https://github.com/allexis096.png" alt="profile" />
-          <strong>#allexis096</strong>
-          <FaArrowRight size={20} />
-        </main>
-      </CardFollowing>
+      {following.map(follow => (
+        <CardFollowing key={follow.id}>
+          <main>
+            <div />
+            <img src={follow.avatar_url} alt="profile" />
+            <strong>#{follow.login}</strong>
+            <Link to="/profile" onClick={() => handleClick(follow.login)}>
+              <FaArrowRight size={20} />
+            </Link>
+          </main>
+        </CardFollowing>
+      ))}
     </Container>
   );
 };
